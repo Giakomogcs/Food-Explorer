907129666b9e259cb67326d9bdfc08a9b12ecad1
Giovani <giakomogcs@gmail.com>
2023-04-24 22:56:35 -0300

    AppError

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,6 +4,13 @@ const UsersController = require("../controllers/UsersController.js")
 
 const usersRoutes = Router()
 
+function MyMiddleware(request, response, next){
+
+}
+
+
+
+
 const usersController = new UsersController()
 
 usersRoutes.post("/", usersController.create)
