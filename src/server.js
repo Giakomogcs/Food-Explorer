e01dc6ab5649362c3f5c40cf9a8eb95a3ac66fe4
Giovani <giakomogcs@gmail.com>
2023-04-24 21:12:03 -0300

    adicionando nodemon

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ const express = require("express")
 
 const app = express()
 
+app.get("/", (response, request) => {
+
+})
+
 const PORT = 3333
 
 app.listen(PORT, ()=> console.log(`Server is running on Port ${PORT}`))
