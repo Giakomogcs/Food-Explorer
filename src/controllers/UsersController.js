21ed90b621a5af9da1d872e16d6aac0eda65c2f8
Giovani <giakomogcs@gmail.com>
2023-04-25 21:49:25 -0300

    Migrations Users

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,7 +1,8 @@
 const AppError = require("../utils/AppError")
+const sqliteConnection = require('../database/sqlite')
 
 class UsersController{
-  create(request,response){
+  async create(request,response){
     const {name, email, password} = request.body
 
     if(!name){
