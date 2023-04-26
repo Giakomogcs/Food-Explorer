3e2e14514d0b2a35e0500d107f3e0013e5a9c8e2
Giovani <giakomogcs@gmail.com>
2023-04-25 22:55:46 -0300

    atualizando Usuario

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -18,7 +18,38 @@ class UsersController{
     await database.run("INSERT INTO users (name, email, password, isAdmin) VALUES (?,?,?,?)",
     [name,email,hashedPassword,isAdmin?isAdmin:false])
 
-    response.status(201).json({name, email, password:hashedPassword, isAdmin})
+    response.status(201).json({name, email, password, isAdmin})
+  }
+
+  async update(request,response){
+    const {name, email, password, isAdmin} = request.body
+    const {id} = request.params
+
+    const database = await sqliteConnection()
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
+
+    if(!user){
+      throw new AppError("Usuário não encontrado")
+    }
+
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id){
+      throw new AppError("Este e-mail já está em uso")
+    }
+    user.name = name
+    user.email = email
+
+    await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      updated_at = ?
+      WHERE id = ?`,
+      [user.name, user.email, new Date(), id]
+    )
+
+    return response.status(200).json("Atualizado com sucesso!")
   }
 }
 module.exports = UsersController
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,15 +4,9 @@ const UsersController = require("../controllers/UsersController.js")
 
 const usersRoutes = Router()
 
-function MyMiddleware(request, response, next){
-
-}
-
-
-
-
 const usersController = new UsersController()
 
 usersRoutes.post("/", usersController.create)
+usersRoutes.put("/:id", usersController.update)
 
 module.exports = usersRoutes
