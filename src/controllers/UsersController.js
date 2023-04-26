4625fa4313b08b10320ab5bef4dd4a7d09b3dd08
Giovani <giakomogcs@gmail.com>
2023-04-25 22:17:06 -0300

    Criptografando senha do usuário

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,15 +1,24 @@
 const AppError = require("../utils/AppError")
 const sqliteConnection = require('../database/sqlite')
+const {hash} = require("bcrypt")
 
 class UsersController{
   async create(request,response){
-    const {name, email, password} = request.body
+    const {name, email, password, isAdmin} = request.body
 
-    if(!name){
-      throw new AppError("Nome é obrigatório")
+    const database = await sqliteConnection()
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+    if(checkUserExists){
+      throw new AppError("Esse e-mail já está em uso")
     }
 
-    response.status(201).json({name, email, password})
+    const hashedPassword = await hash(password, 8)
+
+    await database.run("INSERT INTO users (name, email, password, isAdmin) VALUES (?,?,?,?)",
+    [name,email,hashedPassword,isAdmin?isAdmin:false])
+
+    response.status(201).json({name, email, password:hashedPassword, isAdmin})
   }
 }
 module.exports = UsersController
