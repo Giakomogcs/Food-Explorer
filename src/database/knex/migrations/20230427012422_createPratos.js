7c882cc926bb39ca4246f1f6c3e48246454f3d58
Giovani <giakomogcs@gmail.com>
2023-04-26 22:43:54 -0300

    criando tabela pratos com knex

---
+++ src/database/knex/migrations/20230427012422_createPratos.js
@@ -0,0 +1,15 @@
+
+exports.up = knex => knex.schema.createTable("pratos", table => {
+
+  table.increments("id"),
+  table.text("name"),
+  table.text("category"),
+  table.float("price"),
+  table.text("description")
+  table.integer("user_id").references("id").inTable("users")
+
+  table.timestamp("created_at").default(knex.fn.now())
+  table.timestamp("updated_at").default(knex.fn.now())
+})
+
+exports.down = knex => knex.schema.dropTable("pratos")
