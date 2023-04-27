95d5b0dec75b31a28947be64a34a7b30d2e12182
Giovani <giakomogcs@gmail.com>
2023-04-27 20:27:07 -0300

    migration igredientes

--- src/database/knex/migrations/20230427012422_createPratos.js
+++ src/database/knex/migrations/20230427012422_createPratos.js
@@ -6,6 +6,7 @@ exports.up = knex => knex.schema.createTable("pratos", table => {
   table.text("category"),
   table.float("price"),
   table.text("description")
+  table.text("picture")
   table.integer("user_id").references("id").inTable("users")
 
   table.timestamp("created_at").default(knex.fn.now())
